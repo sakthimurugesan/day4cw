c4c4391ff98ce2aa14dfe62e9c17a6d44c2a0e85
SAKTHI M <msakthi150@gmail.com>
2023-11-18 09:59:36 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import Button from '@mui/material/Button';
 
-function App() {
+const fruits = ['Apple', 'Banana', 'Cherry', 'Durian', 'Elderberry'];
+
+const App = () => {
+  const [name, setName] = useState('');
+  const [selectedFruit, setSelectedFruit] = useState(null);
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSubmitted(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <form onSubmit={handleSubmit}>
+      <TextField
+        label="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        data-testid="name"
+      />
+      <Autocomplete
+        options={fruits}
+        value={selectedFruit}
+        onChange={(_, newValue) => setSelectedFruit(newValue)}
+        renderInput={(params) => (
+          <TextField {...params} label="Choose a fruit" data-testid="autocomplete" />
+        )}
+      />
+      <Button type="submit" variant="contained" data-testid="button">
+        Submit
+      </Button>
+
+      {submitted && (
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello, {name}! Your favorite fruit is {selectedFruit}.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      )}
+    </form>
   );
-}
+};
 
 export default App;
